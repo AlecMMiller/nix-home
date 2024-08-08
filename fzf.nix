dc370ffe41e680b79e6f962fe086e6774fe2cabc
Alec Miller <alecmichaelmiller@gmail.com>
2024-08-08 16:54:35 -0700

    fzf printing

--- fzf.nix
+++ fzf.nix
@@ -3,5 +3,11 @@
 {
   programs.fzf = {
     enable = true;
+    fileWidgetOptions = [
+      "--preview 'bat -n --color=always --line-range :500 {}'"
+    ];
+    changeDirWidgetOptions = [
+      "--preview 'eza --tree --color=always {} | head -200'"
+    ];
   };
 }
