7b1497c9215db1352dcaf03642e505bfe8fdc1b4
Alec Miller <alecmichaelmiller@gmail.com>
2024-09-07 09:09:11 -0700

    telescope ignores node_modules and rust target

--- neovim.nix
+++ neovim.nix
@@ -49,6 +49,17 @@
         type = "lua";
         config = ''
           local builtin = require('telescope.builtin')
+          
+          local telescope = require('telescope')
+
+          telescope.setup {
+            defaults = {
+              file_ignore_patterns = {
+                "node_modules",
+                "target"
+              }
+            }
+          }
           vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
           vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
           vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
