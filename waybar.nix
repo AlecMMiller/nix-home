6990f3f5ede5abd8af4e8d5907d2403467e60355
Alec Miller <alecmichaelmiller@gmail.com>
2024-09-13 06:08:58 -0700

    correct conditional on top bar

--- waybar.nix
+++ waybar.nix
@@ -1,61 +1,64 @@
-{ ... }:
-
+{ config, ... }:
+let
+  primary_monitor = if config.bundles.desktop then "DP-2" else "eDP-1";
+in
 {
-  programs.waybar = {
-    enable = true;
+  config = {
+    programs.waybar = {
+      enable = true;
 
-    settings = {
-      mainBar = {
-        output = ["DP-2" "eDP-1"];
-        layer = "top";
-        position = "top";
-        modules-left = ["mpris"];
-        modules-center = ["hyprland/workspaces"];
-        modules-right = ["pulseaudio" "clock"];
-        clock.format = "{:%H:%M}";
-        mpris = {
-          format = "{artist} - {title}";
-        };
-        pulseaudio = {
-          on-click = "pavucontrol";
-        };
-        "hyprland/workspaces" = {
-          all-outputs = true;
-          format = "{icon}";
-	        format-icons = {
-            "1" = "";
-	          "2" = "";
-            "3" = "";
-	          "4" = "";
-            "7" = "";
-	          "8" = "󰝚";
-	          "9" = "󰍡";
-	        };
+      settings = {
+        mainBar = {
+          output = [primary_monitor];
+          layer = "top";
+          position = "top";
+          modules-left = ["mpris"];
+          modules-center = ["hyprland/workspaces"];
+          modules-right = ["pulseaudio" "clock"];
+          clock.format = "{:%H:%M}";
+          mpris = {
+            format = "{artist} - {title}";
+          };
+          pulseaudio = {
+            on-click = "pavucontrol";
+          };
+          "hyprland/workspaces" = {
+            all-outputs = true;
+            format = "{icon}";
+            format-icons = {
+              "1" = "";
+              "2" = "";
+              "3" = "";
+              "4" = "";
+              "7" = "";
+              "8" = "󰝚";
+              "9" = "󰍡";
+            };
+          };
         };
       };
-    };
 
-    style =
-      let
-      clockColor = "@text";
+      style =
+        let
+        clockColor = "@text";
       makeBorder = color: "border-bottom: 3px solid ${color};";
       makeInfo = color: ''
         color: ${color};
-        ${makeBorder color};
+      ${makeBorder color};
       '';
-    in
-    ''
-      * {
-        border: none;
+      in
+        ''
+        * {
+border: none;
         border-radius: 0;
         font-family: Source Code Pro;
-      }
+        }
       window#waybar {
         background-color: @crust;
       }
-      #clock,
-      #pulseaudio {
-        margin: 0px;
+#clock,
+#pulseaudio {
+margin: 0px;
         padding-top: 1px;
         padding-bottom: 1px;
         padding-left: 6px;
@@ -64,47 +67,48 @@
         margin-top: 4px;
         margin-bottom: 4px;
         border-radius: 20px 0px 0px 20px;
-      }
-      #workspaces {
-        background-color: @base;
-        padding-top: 1px;
-        padding-bottom: 1px;
-        margin-top: 4px;
-        margin-bottom: 4px;
-        border-radius: 20px;
-      }
-      #workspaces button {
-        color: @text;
-      }
-      #workspaces button.empty {
-        color: @overlay1;
-      }
-      #workspaces button.visible {
-        color: @teal;
-      }
-      #workspaces button.hosting-monitor {
-        color: @green;
-      }
-      #pulseaudio {
-        padding-left: 12px;
-      }
-      #mpris {
-        color: @text;
-        background-color: @base;
-        margin-top: 4px;
-        margin-bottom: 4px;
-        padding-left: 12px;
-        padding-right: 12px;
-        border-radius: 20px;
-        margin-left: 8px;
-      }
-      #clock {
-        color: @text;
-        border-radius: 0px 20px 20px 0px;
-        padding-right: 12px;
-        margin-right: 8px;
-      }
-    '';
+}
+#workspaces {
+  background-color: @base;
+  padding-top: 1px;
+  padding-bottom: 1px;
+  margin-top: 4px;
+  margin-bottom: 4px;
+  border-radius: 20px;
+}
+#workspaces button {
+color: @text;
+}
+#workspaces button.empty {
+color: @overlay1;
+}
+#workspaces button.visible {
+color: @teal;
+}
+#workspaces button.hosting-monitor {
+color: @green;
+}
+#pulseaudio {
+  padding-left: 12px;
+}
+#mpris {
+color: @text;
+       background-color: @base;
+       margin-top: 4px;
+       margin-bottom: 4px;
+       padding-left: 12px;
+       padding-right: 12px;
+       border-radius: 20px;
+       margin-left: 8px;
+}
+#clock {
+color: @text;
+       border-radius: 0px 20px 20px 0px;
+       padding-right: 12px;
+       margin-right: 8px;
+}
+'';
 
-  };
+};
+};
 }
