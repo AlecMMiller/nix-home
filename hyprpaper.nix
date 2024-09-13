dc250a3ee6b156f623555397d97940a9d5a67cfb
Alec Miller <alecmichaelmiller@gmail.com>
2024-09-13 06:19:14 -0700

    wallpaper on laptop

--- hyprpaper.nix
+++ hyprpaper.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ ... }:
 
 {
   services.hyprpaper = {
@@ -9,12 +9,14 @@
       preload = [
         "~/Pictures/3.jpg"
         "~/Pictures/2.jpg"
+          "NVD_BACKEND,direct"
         "~/Pictures/4.jpg"
       ];
       wallpaper = [
         "DP-1,~/Pictures/2.jpg"
         "DP-2,~/Pictures/4.jpg"
         "HDMI-A-1,~/Pictures/3.jpg"
+        "eDP-1,~/Pictures/2.jpg"
       ];
     };
   };
