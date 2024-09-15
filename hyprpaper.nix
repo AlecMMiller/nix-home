c9cd025666539d8a1d306a9b3f8308bdbc3ecdf6
Alec Miller <alecmichaelmiller@gmail.com>
2024-09-14 18:45:29 -0700

    include wallpaper in repo

--- hyprpaper.nix
+++ hyprpaper.nix
@@ -7,16 +7,16 @@
       ipc = "off";
       splash = false;
       preload = [
+        "~/.config/home-manager/media/1.jpg"
         "~/Pictures/3.jpg"
         "~/Pictures/2.jpg"
-          "NVD_BACKEND,direct"
         "~/Pictures/4.jpg"
       ];
       wallpaper = [
         "DP-1,~/Pictures/2.jpg"
         "DP-2,~/Pictures/4.jpg"
         "HDMI-A-1,~/Pictures/3.jpg"
-        "eDP-1,~/Pictures/2.jpg"
+        "eDP-1,~/.config/home-manager/media/1.jpg"
       ];
     };
   };
