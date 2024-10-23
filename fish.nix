54fda92b67b53f3959bcefd35e1f30116b3077ea
Alec Miller <alecmichaelmiller@gmail.com>
2024-10-23 06:13:27 -0700

    use vi key bindings

--- fish.nix
+++ fish.nix
@@ -4,11 +4,12 @@
   programs.fish = {
     enable = true;
     shellAliases = {
-      "nix-re"="sudo nixos-rebuild switch";
-      "home-re"="home-manager switch --impure";
-      ls="eza --color=always --long --no-filesize --icons=always --no-time --no-user --no-permissions";
+      "nix-re" = "sudo nixos-rebuild switch";
+      "home-re" = "home-manager switch --impure";
+      ls = "eza --color=always --long --no-filesize --icons=always --no-time --no-user --no-permissions";
     };
     interactiveShellInit = ''
+      fish_vi_key_bindings
       set fish_greeting
     '';
   };
