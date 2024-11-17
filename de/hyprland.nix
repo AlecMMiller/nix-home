89517e7c9e09894605ebde3b75cb541b5354510c
Alec Miller <alecmichaelmiller@gmail.com>
2024-11-17 14:58:57 -0700

    no gaps when only

--- de/hyprland.nix
+++ de/hyprland.nix
@@ -107,6 +107,15 @@ in
           "workspace 9 silent,class:^(discord)$"
           "workspace 4 silent,class:^(cortex)$"
           "tile,class:^(blender)$"
+          "bordersize 0, floating:0, onworkspace:w[tv1]"
+          "rounding 0, floating:0, onworkspace:w[tv1]"
+          "bordersize 0, floating:0, onworkspace:f[1]"
+          "rounding 0, floating:0, onworkspace:f[1]"
+        ];
+
+        workspace = [
+          "w[tv1], gapsout:0, gapsin:0"
+          "f[1], gapsout:0, gapsin:0"
         ];
 
         "$mod" = "SUPER";
