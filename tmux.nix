890e4d95c2fb66b080e8c972011bb7ce050b0b0b
Alec Miller <alecmichaelmiller@gmail.com>
2024-12-07 10:14:22 -0700

    shortcut to open home manager config

--- tmux.nix
+++ tmux.nix
@@ -5,9 +5,13 @@
     enable = true;
     extraConfig = ''
       set -g base-index 1 
+
       bind r source-file ~/.config/tmux/tmux.conf
+
       bind-key -r f run-shell "tmux neww ~/.scripts/sessionizer.sh"
       bind-key -r i run-shell "tmux neww ~/.scripts/cht.sh"
+
+      bind-key -r H run-shell "~/.scripts/sessionizer.sh ~/.config/home-manager"
     '';
   };
 }
